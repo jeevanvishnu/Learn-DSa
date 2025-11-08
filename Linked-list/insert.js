0299e3aa8bbf5d5d17dba0f06466b97c6f0f26fc
jeevanvishnu <jeevanvishnu18@gmail.com>
2025-11-09 00:40:39 +0530

    Refactor LinkedList class: add deleteLastNode, search, reverse, and clear methods

--- Linked-list/insert.js
+++ Linked-list/insert.js
@@ -37,6 +37,56 @@ class LinkedList{
         
         this.head = this.head.next
     }
+    deleteLastNode(){
+        if(!this.head){
+            console.log("List is empty");
+            return
+        }
+
+        if(!this.head.next){
+            this.head = null
+            return
+        }
+
+        let curent = this.head
+        while(curent.next.next){
+            curent = curent.next
+        }
+        curent.next = null
+    }
+
+    search(data){
+       let current = this.head
+       let postion = 0
+        while(current){
+            if(current.data === data){
+                console.log(`${postion} data found postion`);
+            
+            }
+            current = current.next
+            postion++
+        }
+        console.log("Not Found");
+        
+    }
+
+    reverse(){
+        let prev = null
+        let current = this.head
+        let next = null
+
+        while(current){
+            next = current.next // 20
+            current.next = prev 
+            prev = current  // 10
+            current = next
+        }
+        this.head =prev
+    }
+
+    clear(){
+        this.head = null
+    }
 
     print(){
         let current = this.head
@@ -50,7 +100,10 @@ class LinkedList{
         console.log(result + 'null');
         
     }
+
 }
+
+
 
 const list = new LinkedList()
 
@@ -58,5 +111,9 @@ list.append(10)
 list.append(20)
 list.insertFirstNode(15)
 list.append(30)
+list.append(40)
+list.search(2)
+list.reverse()
 list.deleteFirstNode()
+list.deleteLastNode()
 list.print()
